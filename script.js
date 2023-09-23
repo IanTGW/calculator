badaddf987aa0219c579a148c993f75e8a30859b
IanTGW <ianwuxiinau@gmail.com>
2023-09-24 09:10:36 +1000

    Add calculator functionality with error handling

--- script.js
+++ script.js
@@ -1,33 +1,107 @@
 function add(a, b){
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b){
-    return a - b;
+    return Number(a) - Number(b);
 }
 
 function multiply(a, b){
-    return a * b;
+    return Number(a) * Number(b);
 }
 
 function divide(a, b){
-    return a / b;
+    return Number(a) / Number(b);
 }
 
-let a;
-let op;
-let b;
+function equals(a, b){
+    return Number(a);
+}
+
+let a = 0;
+let op = "+";
+let b = 0;
 
 function operate(a, op, b){
     if (op === "+"){
-       return add(a, b);
+        return add(a, b);
     } else if (op === "-"){
-       return subtract(a, b);
-    } else if (op === "*"){
-       return multiply(a, b);
-    } else if (op === "/"){
-       return divide(a, b);
+        return subtract(a, b);
+    } else if (op === "×"){
+        return multiply(a, b);
+    } else if (op === "÷"){
+        return divide(a, b);
+    } else if (op === "="){
+        return equals(a, b);
     }
 }
 
-console.log(operate(45, "*", 63))
+function rounded(number){
+    if(number.toString().length > 11){
+        return Number(number).toPrecision(7);
+    } else {
+        return Number(number);
+    }
+}
+
+let lastType = "number";
+
+function buttonPress(){
+    if(this.classList.contains("number")){
+            if(b === 0){
+                b = this.textContent
+                displayValue.textContent = b;
+            } else {
+                b += this.textContent;
+                displayValue.textContent = rounded(b);
+            }
+            lastType = "number";
+    } else if(this.classList.contains("operator")){
+            if (lastType === "arithmetic" && this.id === "eval"){
+                displayValue.textContent = "Enter a value!";
+                lastType = "operator";
+            } else if(lastType === "operator" && this.id === "eval"){
+                displayValue.textContent = "Enter a value!";
+                lastType = "operator";
+            } else if(lastType === "operator"){
+                displayValue.textContent = rounded(a);
+                op = this.textContent;
+                lastType = "operator";
+            } else {
+                console.log(b)
+                a = operate(a, op, b);
+                b = 0;
+                op = this.textContent;
+                if( a === Infinity || a === -Infinity || isNaN(a)){
+                    displayValue.textContent = "Error"
+                } else if( op === "=" ){
+                    displayValue.textContent = rounded(equals(a, b));
+                } else {
+                    displayValue.textContent = rounded(a);
+                }
+                console.log(lastType)
+                if(this.classList.contains("arithmetic")){
+                    lastType = "arithmetic";
+                } else {
+                    lastType = "operator";
+                }
+            }
+    } else if (this.classList.contains("clear")){
+            b = 0;
+            displayValue.textContent = b;
+    } else if (this.classList.contains("allClear")){
+            a = 0;
+            op = "+";
+            b = 0;
+            displayValue.textContent = b;
+    }
+}
+
+let displayValue = document.querySelector(".result");
+
+let buttons = document.querySelectorAll(".button")
+
+for(let i = 0; i < buttons.length; i++){ 
+    buttons[i].addEventListener("click", buttonPress)
+};
+
